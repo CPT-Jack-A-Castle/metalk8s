f8b83ed16372e16a010022c41d68028e1038c17a
Alexis Le Dinh <alexisledinh@gmail.com>
2021-06-11 10:38:48 +0200

    ui, alerts: Update AlertProvider to use ConfigProvider instead of redux
    Refs: #3256

--- ui/src/containers/AlertProvider.js
+++ ui/src/containers/AlertProvider.js
@@ -10,17 +10,34 @@ import React, {
 import { useQuery } from 'react-query';
 import { useTypedSelector } from '../hooks';
 import { ErrorBoundary } from 'react-error-boundary';
-import type { FilterLabels } from '../services/alertUtils';
+import type { FilterLabels, Alert } from '../services/alertUtils';
 import { ErrorPage500 } from '@scality/core-ui';
+import { useConfig } from './ConfigProvider';
+import { STATUS_HEALTH } from '../constants';
 
 export const useAlerts = (filters: FilterLabels) => {
-  const alertsVersion = useTypedSelector(
-    (state) => state.config.api?.alerts_lib_version,
-  );
+  const { alerts_lib_version } = useConfig();
 
-  if (window.shellUIAlerts && window.shellUIAlerts[alertsVersion]) {
-    return window.shellUIAlerts[alertsVersion].useAlerts(useContext)(filters);
+  if (window.shellUIAlerts && window.shellUIAlerts[alerts_lib_version]) {
+    return window.shellUIAlerts[alerts_lib_version].useAlerts(useContext)(
+      filters,
+    );
   }
+};
+
+export const useHighestSeverityAlerts = (filters: FilterLabels) => {
+  const { alerts_lib_version } = useConfig();
+
+  if (window.shellUIAlerts && window.shellUIAlerts[alerts_lib_version]) {
+    return window.shellUIAlerts[alerts_lib_version].useHighestSeverityAlerts(
+      useContext,
+      filters,
+    );
+  }
+};
+
+export const highestAlertToStatus = (alerts?: Alert[]): Status => {
+  return (alerts?.[0] && ((alerts[0].severity: any): Status)) || STATUS_HEALTH;
 };
 
 /**
@@ -32,7 +49,7 @@ export const useAlerts = (filters: FilterLabels) => {
  * @param {string} src library bundle url
  * @param {string?} version library version
  */
-function useAlertLibrary(src?: string, version?: string) {
+function useLoadAlertLibrary(src?: string, version?: string) {
   const [hasFailed, setHasFailed] = useState(false);
   useLayoutEffect(() => {
     const body = document.body;
@@ -49,8 +66,8 @@ function useAlertLibrary(src?: string, version?: string) {
         setHasFailed(true);
       };
       scriptElement.onload = () => {
-        window.shellUIAlerts[version].createAlertContext(createContext)
-      }
+        window.shellUIAlerts[version].createAlertContext(createContext);
+      };
       body.appendChild(scriptElement);
     }
   }, [src, version]);
@@ -60,28 +77,43 @@ function useAlertLibrary(src?: string, version?: string) {
   }
 }
 
-const InternalAlertProvider = ({ children }: { children: Node }): Node => {
-  const alertsUrl = useTypedSelector((state) => state.config.api?.url_alerts);
-  const alertsVersion = useTypedSelector(
-    (state) => state.config.api?.alerts_lib_version,
-  );
-  const alertManagerUrl = useTypedSelector(
-    (state) => state.config.api?.url_alertmanager,
-  );
+export class AlertLibraryNotAvailable extends Error {
+  constructor() {
+    super('Platform library is not available');
+  }
+}
 
-  useAlertLibrary(alertsUrl, alertsVersion);
+export function useAlertLibrary() {
+  const { alerts_lib_version } = useConfig();
+
+  if (window.shellUIAlerts && window.shellUIAlerts[alerts_lib_version]) {
+    return window.shellUIAlerts[alerts_lib_version];
+  }
+
+  throw new AlertLibraryNotAvailable();
+}
+
+const InternalAlertProvider = ({ children }: { children: Node }): Node => {
+  const { url_alertmanager, url_alerts, alerts_lib_version } = useConfig();
+
+  useLoadAlertLibrary(url_alerts, alerts_lib_version);
 
   const AlertProviderRef = useRef(null);
 
   // In order to avoid react to uselessly rerender children every time we move
   // the AlertProvider to a ref and renders it only a single time once the library is ready.
-  if (window.shellUIAlerts && window.shellUIAlerts[alertsVersion] && !AlertProviderRef.current) {
-    AlertProviderRef.current = window.shellUIAlerts[alertsVersion].AlertProvider(useQuery);
+  if (
+    window.shellUIAlerts &&
+    window.shellUIAlerts[alerts_lib_version] &&
+    !AlertProviderRef.current
+  ) {
+    AlertProviderRef.current =
+      window.shellUIAlerts[alerts_lib_version].AlertProvider(useQuery);
   }
 
   if (AlertProviderRef.current) {
     return (
-      <AlertProviderRef.current alertManagerUrl={alertManagerUrl}>
+      <AlertProviderRef.current alertManagerUrl={url_alertmanager}>
         {children}
       </AlertProviderRef.current>
     );
@@ -93,7 +125,13 @@ const InternalAlertProvider = ({ children }: { children: Node }): Node => {
 function ErrorFallback() {
   const { language, api } = useTypedSelector((state) => state.config);
   const url_support = api?.url_support;
-  return <ErrorPage500 data-cy='sc-error-page500' locale={ language } supportLink={ url_support }/>;
+  return (
+    <ErrorPage500
+      data-cy="sc-error-page500"
+      locale={language}
+      supportLink={url_support}
+    />
+  );
 }
 
 const AlertProvider = ({ children }: { children: Node }): Node => {
